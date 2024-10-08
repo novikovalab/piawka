26ab0db343a17ec6344b7ab170ab1c2c214a3fe1
Nikita Tikhomirov <nikita.p.tikhomirov@gmail.com>
2024-10-08 09:30:32 +0200

    simplify conda recipe

--- .conda_recipe/build.sh
+++ .conda_recipe/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# increase verbosity for debugging
-set -x
-
 mkdir -p $PREFIX/bin
-
-conda env config vars set AWKLIBPATH="$PREFIX/lib/gawk"
-conda env config vars set AWKPATH="$PREFIX/bin/"
 cp $SRC_DIR/scripts/* $PREFIX/bin/* 
 chmod +x $PREFIX/bin/*
 
